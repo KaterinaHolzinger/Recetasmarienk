f26fca5a5d4850198f348c1fd291242e45c45762
KaterinaHolzinger <holziinger@gmail.com>
2025-05-28 12:56:09 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,13 @@
+function iniciarsesion() {
+    const arrayusers = ["manfredo", "ines", "admin"];
+    const contraseña = document.getElementById("password").value;
+    const username = document.getElementById("username").value;
+
+    if (arrayusers.includes(username) && contraseña === "marienk") {
+        document.location.href = "secreto.html";
+    } else {
+        const mensaje= document.getElementById("mensaje");
+        mensaje.textContent = "contraseña o usuario incorrecto";
+        mensaje.style.color = "red";
+    }
+}
